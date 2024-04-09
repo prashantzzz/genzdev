f883b28b557258c6082eefd8ef4c08a83444e8a4
Prashantzzz <prashant2003prashant@gmail.com>
2024-04-10 01:18:39 +0530

    updated ui and logic

--- script.js
+++ script.js
@@ -1,34 +1,25 @@
-/*
-Live Code Editor 
-By: Coding Design
-
-You can do whatever you want with the code. However if you love my content, you can subscribed my YouTube Channel
-🌎link: www.youtube.com/codingdesign
-
-*/
-
-const html_code = document.querySelector('.html-code textarea');
-const css_code = document.querySelector('.css-code textarea');
-const js_code = document.querySelector('.js-code textarea');
+const htmlCode = document.querySelector('.html-code textarea');
+const cssCode = document.querySelector('.css-code textarea');
+const jsCode = document.querySelector('.js-code textarea');
 const result = document.querySelector('#result');
 
 function run() {
     // Storing data in Local Storage
-    localStorage.setItem('html_code', html_code.value);
-    localStorage.setItem('css_code', css_code.value);
-    localStorage.setItem('js_code', js_code.value);
+    localStorage.setItem('htmlCode', htmlCode.value);
+    localStorage.setItem('cssCode', cssCode.value);
+    localStorage.setItem('jsCode', jsCode.value);
 
     // Executing HTML, CSS & JS code
-    result.contentDocument.body.innerHTML = `<style>${localStorage.css_code}</style>` + localStorage.html_code;
-    result.contentWindow.eval(localStorage.js_code);
+    result.contentDocument.body.innerHTML = `<style>${localStorage.cssCode}</style>` + localStorage.htmlCode;
+    result.contentWindow.eval(localStorage.jsCode);
 }
 
 // Checking if user is typing anything in input field
-html_code.onkeyup = () => run();
-css_code.onkeyup = () => run();
-js_code.onkeyup = () => run();
+htmlCode.onkeyup = () => run();
+cssCode.onkeyup = () => run();
+jsCode.onkeyup = () => run();
 
 // Accessing data stored in Local Storage. To make it more advanced you could check if there is any data stored in Local Storage.
-html_code.value = localStorage.html_code;
-css_code.value = localStorage.css_code;
-js_code.value = localStorage.js_code;
+htmlCode.value = localStorage.htmlCode;
+cssCode.value = localStorage.cssCode;
+jsCode.value = localStorage.jsCode;
